8610c31e7e037d6c9d6cd014ed015b793d945c8c
nicros <nik24012002@gmail.com>
2023-10-03 17:14:53 +0700

    added inits

--- homework.py
+++ homework.py
@@ -1,7 +1,22 @@
+from dataclasses import dataclass
+
+M_IN_KM = 1000
+LEN_STEP = (0.65, 1.38)
+
+
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    pass
+    @dataclass
+    def __init__(self) -> None:
+        training_type : str
+        duration : float
+        distance : float
+        speed : float
+        calories : str
 
+    def get_message(self):
+        return f'Тип тренировки: {self.training_type}; Длительность: {self.duration:.2f} ч.; Дистанция: {self.distance:.2f} км; Ср. скорость: {self.speed:.2f} км/ч; Потрачено ккал: {self.calories:.2f}.'
+        
 
 class Training:
     """Базовый класс тренировки."""
@@ -15,11 +30,11 @@ class Training:
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
-        pass
+         return self.action * LEN_STEP / M_IN_KM
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        pass
+        return self.get_distance() / self.duration
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
@@ -27,22 +42,26 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
+        return InfoMessage
         pass
 
 
 class Running(Training):
     """Тренировка: бег."""
-    pass
+    def __init__(self, action: int, duration: float, weight: float, LEN_STEP = 0.65) -> None:
+        super().__init__(action, duration, weight)
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    pass
+    def __init__(self, action: int, duration: float, weight: float, LEN_STEP = 0.65) -> None:
+        super().__init__(action, duration, weight)
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
-    pass
+    def __init__(self, action: int, duration: float, weight: float, LEN_STEP = 1.38) -> None:
+        super().__init__(action, duration, weight)
 
 
 def read_package(workout_type: str, data: list) -> Training:
